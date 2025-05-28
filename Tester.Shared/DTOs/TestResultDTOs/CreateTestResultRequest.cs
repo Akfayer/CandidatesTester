f3eeb042548b640d6c9ca36cba02f1006a3bb2b7
Akfayer <denefer31@gmail.com>
2025-05-28 20:37:04 +0300

    TestResult

---
+++ Tester.Shared/DTOs/TestResultDTOs/CreateTestResultRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tester.Shared.DTOs.TestResultDTOs
+{
+    public class CreateTestResultRequest
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int TestId { get; set; }
+
+        [Required]
+        public int Score { get; set; }
+
+        [Required]
+        public int MaxScore { get; set; }
+    }
+}
---
+++ Tester.Shared/DTOs/TestResultDTOs/TestResultResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Tester.Shared.DTOs.TestResultDTOs;
+
+public class TestResultResponse
+{
+    [JsonPropertyName("questionId")]
+    public int TestResultId { get; set; }
+    [JsonPropertyName("userId")]
+    public int UserId { get; set; }
+    [JsonPropertyName("testId")]
+    public int TestId { get; set; }
+    [JsonPropertyName("score")]
+    public int Score { get; set; }
+    [JsonPropertyName("maxScore")]
+    public int MaxScore { get; set; }
+    [JsonPropertyName("completedAt")]
+    public DateTime CompletedAt { get; set; }
+}
