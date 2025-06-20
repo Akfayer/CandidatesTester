e9418b13f44b355075e3e84a6115b06d5b0498a0
Akfayer <denefer31@gmail.com>
2025-06-21 00:16:42 +0300

    Commit staged

---
+++ Tester.Shared/DTOs/TestResultDTOs/CheckTestRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tester.Shared.DTOs.UserAnswerDTOs;
+
+namespace Tester.Shared.DTOs.TestResultDTOs;
+
+public class CheckTestRequest
+{
+    [Required]
+    public int UserId { get; set; }
+
+    [Required]
+    public int TestId { get; set; }
+
+    [Required]
+    public List<SubmitUserAnswerRequest> Answers { get; set; }
+}
